7abdfbf17f084a7d9d7db72607836b5a11e2bd43
Gianhunter00 <andrea00@botti.org>
2022-03-31 03:55:44 +0200

    Refactor Circle
    Removed print test in circle class

--- CircleExercise.py
+++ CircleExercise.py
@@ -20,21 +20,4 @@ class Circle(object):
     
     def sort(self):
         return self.radius
-    
-    
-circle_one = Circle(5)
-circle_two = Circle(5)
-circle_three = Circle(10)
-
-print(circle_one == circle_two)
-print(circle_three > circle_two)
-
-circle_add = circle_one + circle_three
-
-print(circle_add)
-
-circle_list = [circle_one, circle_two, circle_three, circle_add]
-circle_list.sort(key=Circle.sort)
-
-print(*circle_list)
 
